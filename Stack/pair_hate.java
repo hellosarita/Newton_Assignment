527360d8c90b418b5348b6728fc411da36d1a490
hellosarita <109722610+hellosarita@users.noreply.github.com>
2022-10-08 21:09:04 +0530

    Create pair_hate.java

---
+++ Stack/pair_hate.java
@@ -0,0 +1,28 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+                      Scanner sc = new Scanner(System.in);
+                      String str = sc.next();
+                      
+                    Stack<Character>st = new Stack<Character>();
+                      for (int i = 0; i<str.length();i++){
+                          char ch = str.charAt(i);
+                          if ( st.isEmpty( )== false && st.peek()== ch){
+                              st.pop();
+                          }
+                          else {
+                              st.push(ch);
+                          }
+                      }
+                      StringBuilder sb = new StringBuilder();
+                      while (!st.isEmpty()){
+                          sb.append(st.pop());
+                      }
+                      System.out.println(sb.reverse());
+    }
+}
